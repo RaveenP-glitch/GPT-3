79f6c0018b7e1fd902f745bc0c8193463daf0bea
Raveen Panditha <raveenpanditha8@gmail.com>
2021-02-09 23:12:29 +0530

    changes

--- ReactJS/react-task-app/src/App.js
+++ ReactJS/react-task-app/src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import Header from './components/Header'
 
 import './App.css';
 
-function App() {
-  const name = "Raveen";
-  const x = false
-
-  return (
-    <div className="App">
-     <h1>Hello from React!..</h1>
-     <h2>Hello {x? 'Yes':'No'}</h2>
+const App = () => {
+  return(
+    <div className = 'container'>
+     <Header/>
     </div>
   )
+
 }
+
+
 
 export default App;
--- ReactJS/react-task-app/src/components/Header.js
+++ ReactJS/react-task-app/src/components/Header.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
-        <div>
-            
-        </div>
+        <header class='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello'/>
+            <Button color='blue' text='Hello1'/>
+            <Button color='red' text='Hello2'/>
+
+        </header>
+       
     )
 }
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
