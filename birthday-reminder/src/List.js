26f0dcfeac08a798813986bb58d84f1484338594
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-11-14 02:09:15 +0530

    finsih bday

--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function List() {
+const List = ({people}) => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <>
+        {people.map((person) => {
+            const {id, name, age, image} = person;
+            return( <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+            </div>
 
-export default List
+            </article>
+            );
+
+        })}
+      </>
+    );
+};
+
+export default List;
--- birthday-reminder/src/data.js
+++ birthday-reminder/src/data.js
@@ -1,11 +1,34 @@
-import React from 'react'
+export default[
+    {
+        id:1,
+        name: 'Bertie Yates',
+        age: 29,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh04_WmVbKGyT6-Zu4Gp7AqgMZYsRRnQic3g&usqp=CAU', 
 
-function data() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    },
+    {
+        id:2,
+        name: 'Hester Hogan',
+        age: 32,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_HlunVidwZfUIr6dFqhBGYBSKD4ZZvnJefA&usqp=CAU'
 
-export default data
+
+    },
+    {
+        id:3,
+        name: 'Larry Little',
+        age: 36,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_cRgcPxzb4mpHMnALHgtN6NwMR55XdOnSag&usqp=CAU'
+
+
+    },
+    {
+        id:4,
+        name: 'Surgey Brin',
+        age: 33,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYzDV9oMHp7mVm2XIKVfgIoVBUrIcNnBgzgw&usqp=CAU'
+
+
+    }
+
+]
