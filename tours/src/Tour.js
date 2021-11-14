3007d5445619421490ffe16906e046e2004ffd0c
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-11-14 15:29:00 +0530

    setup tourss

--- tours/src/Tour.js
+++ tours/src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({id,image,info,price,name}) => {
+    const [readMore, setReadMore] = useState(false)
+
   return ( <article className="single-tour">
       <img src={image} alt={name}/>
       <footer>
@@ -9,7 +11,9 @@ const Tour = ({id,image,info,price,name}) => {
             <h4 className='tour-price'>${price}</h4>
             
           </div>
-          <p className="tour-info">{info}</p>
+          <p className="tour-info">
+              {readMore?info:`${info.substring(0.200)}...`
+          }</p>
           <button className="delete-btn">Not interested</button>
       </footer>
   </article>
