26b1233c2af61fbb5ad103f2e79bbec64ff3d832
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-12-25 00:16:30 +0530

    adding anchors

--- website/src/components/navbar/Navbar.jsx
+++ website/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 //BEM -> Block Element Modifier
@@ -11,6 +12,13 @@ const Navbar = () => {
           <div className = "gpt3__navbar-links_logo">
           <img src={logo}></img>
 
+          </div>
+          <div className="gpt1__navbar-links_container">
+            <p><a href = "home">Home</a></p>
+            <p><a href = "wgpt3">What is GPT?</a></p>
+            <p><a href = "possibility">Open AI</a></p>
+            <p><a href = "features">Case Studies</a></p>
+            <p><a href = "blog">Library</a></p>
           </div>
           </div>
             
